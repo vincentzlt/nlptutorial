72be326ec4fabfc486379136f3a8f656cd66c734
vincentzlt <vincentzlt@gmail.com>
2017-05-02 16:59:12 +0900

    add descending sort

--- download/00-intro/word-count.py
+++ download/00-intro/word-count.py
@@ -9,7 +9,7 @@ from collections import defaultdict
 
 def word_f(f_name):
     try:
-        with open(f_name, "r",encoding="UTF-8") as f:
+        with open(f_name, "r", encoding="UTF-8") as f:
             lines = f.readlines()
     except FileNotFoundError:
         print("there is no such file.")
@@ -18,11 +18,19 @@ def word_f(f_name):
     w_freq_dict = defaultdict(lambda: 0)
     for i in lines:
         for j in i.split():
-            w_freq_dict[j]+=1
+            w_freq_dict[j] += 1
     return w_freq_dict
 
-if __name__=="__main__":
-    w_freq_dict=word_f(sys.argv[1])
-    for i,j in w_freq_dict.items():
+
+def sort_defalutdict(input_dict):
+    """
+    input:  default dict
+    ouput:  sorted list of tuple by values"""
+    tuple_list = sorted(input_dict.items(), key=lambda x:x[1], reverse=True)
+    return tuple_list
+
+if __name__ == "__main__":
+    w_freq_tuple = sort_defalutdict((word_f(sys.argv[1])))
+    for i, j in w_freq_tuple:
         print(i, "\t", str(j))
-    print("# of types:\t", len(w_freq_dict))
+    print("# of types:\t", len(w_freq_tuple))
