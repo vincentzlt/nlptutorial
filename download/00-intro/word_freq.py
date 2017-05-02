cbe54fc10fc51e3277ea6cb1c0af8dfccfa1696c
vincentzlt <vincentzlt@gmail.com>
2017-05-02 11:45:05 +0900

    practice00

---
+++ download/00-intro/word_freq.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# python word_freq.py <filename>
+
+import sys
+from collections import defaultdict
+
+
+def word_f(f_name):
+    try:
+        with open(f_name, "r") as f:
+            lines = f.readlines()
+    except FileNotFoundError:
+        print("there is no such file.")
+        return
+
+    w_freq_dict = defaultdict(lambda: 0)
+    for i in lines:
+        for j in i.split():
+            w_freq_dict[j]+=1
+    return w_freq_dict
+
+if __name__=="__main__":
+    w_freq_dict=word_f(sys.argv[1])
+    for i,j in w_freq_dict.items():
+        print(i, "\t", str(j))
+    print("# of types:\t", len(w_freq_dict))
