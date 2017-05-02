25fc337ffbae7bd3c3274221b1d5e369cab8c4c5
vincentzlt <vincentzlt@gmail.com>
2017-05-02 17:47:49 +0900

    unfinished

--- download/01-unigramlm/train-unigram.py
+++ download/01-unigramlm/train-unigram.py
@@ -1,35 +1,41 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import sys
+import sys,math
 from collections import defaultdict
+
 
 def preprocess_corpus(f_name):
     """
     input:  a file name
     ouput:  a list of lines in the corpus
             each line is a list"""
-    
+
     try:
-        with open(f_name,"r",encoding="UTF-8") as f:
-            lines=f.readlines()
+        with open(f_name, "r", encoding="UTF-8") as f:
+            lines = f.readlines()
     except FileNotFoundError:
         print("file not found. try again")
     except:
         print(sys.exc_info())
-    
-    output=[(i+" </s>").split() for i in lines]
+
+    output = [(i + " </s>").split() for i in lines]
 
     return output
 
 
-if __name__=="__main__":
+def calc_w_uni_P(w, corpus_list,g,vocab_number):
+    sum_token = sum([sum(i) for i in corpus_list])
+    sum_w = sum([sum(filter(lambda w_:w_=w, i)) for i in corpus_list])
+    output=g*sum_w/float(sum_token)+(1-g)/float(n)
+    return output
+
+def calc_entropy(input_list,corpus_list):
+    sum_token=sum([sum(i) for i in corpus_list])
+
+
+if __name__ == "__main__":
     if sys.argv[1]:
         preprocess_corpus(sys.argv[1])
     else:
         print("please enter a file name.")
-
-
-    
-
-
